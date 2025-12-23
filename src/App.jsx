1cab3820b0ecf051d50ece29c647de6544ccea0a
AjayPeter865 <ajaypeter865@gmail.com>
2025-12-23 19:43:55 +0530

    functional componet

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,28 @@ function App() {
 
 }
 
+const FunctionalComponet = () => {
+
+  const [count, setCount] = useState(0)
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <>
+      <div style={{ margin: '50px' }}>
+        <h1>Hello from FunctionalComponet</h1>
+        <h3>Counter app</h3>
+        <h2>{count}</h2>
+        <button onClick={increase}>Add</button>
+      </div>
+    </>
+  )
+
+}
 
 
 
-export default App
+
+export default FunctionalComponet
