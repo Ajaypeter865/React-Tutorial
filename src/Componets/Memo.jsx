da28809c292670c108f87a552b9ee6d3e9727adb
AjayPeter865 <ajaypeter865@gmail.com>
2026-01-19 12:41:03 +0530

    Memo update

--- src/Componets/Memo.jsx
+++ src/Componets/Memo.jsx
@@ -6,13 +6,21 @@ const SquareMemo = () => {
     const [num, setNum] = useState([])
 
     const value = useMemo(() => {
+        console.log('Calculating...');
+        
        return num * num
     }, [num])
+
+    const handleClick = () => {
+        console.log('Clicked');
+        
+    }
 
     return (
         <div>
             <input type="text" placeholder="Enter a number" onChange={(e) => { setNum(e.target.value) }} />
             <h2>Square of the number : {value}</h2>
+            <button onClick={handleClick}>Press button</button>
         </div>
     )
 }
