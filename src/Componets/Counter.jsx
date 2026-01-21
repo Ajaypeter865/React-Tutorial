3bc3c294d045abf53b2dc620b22620742bf9baa5
AjayPeter865 <ajaypeter865@gmail.com>
2026-01-21 19:18:56 +0530

    update

--- src/Componets/Counter.jsx
+++ src/Componets/Counter.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react"
 
+// const Counter = () => {
+//     console.log('Render Counter');
+
+//   const [count, setCount] = useState(0)
+
+//   const Increase = count + 1
+//   const Decrease = count - 1
+//   return (
+//     <div className='btn'>
+//       <h1>Count : {count}</h1>
+//       <button className='btn' onClick={() => {
+//         setCount(Increase)
+
+//       }}>Increase</button>
+
+//       <button onClick={() => {
+//         setCount(Decrease)
+//       }}>Decrease</button>
+//     </div>
+//   )
+// }
+
+
 const Counter = () => {
-    console.log('Render Counter');
 
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState()
   const Increase = count + 1
   const Decrease = count - 1
+
   return (
-    <div className='btn'>
-      <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => {
-        setCount(Increase)
-
-      }}>Increase</button>
-
-      <button onClick={() => {
-        setCount(Decrease)
-      }}>Decrease</button>
+    <div>
+      <button onClick={setCount(Increase)}>Increase</button>
+      <button onClick={setCount(Decrease)}>Decrease</button>
     </div>
   )
 }
--- src/Componets/CounterReducer.jsx
+++ src/Componets/CounterReducer.jsx
@@ -1,32 +1,57 @@
 import { useReducer } from "react"
 
-const reducer = (state, action) => {
-    if (action === 'increment') {
+// const reducer = (state, action) => {
+//     if (action === 'increment') {
 
+//         return state + 1
+//     } else {
+//         return state - 1
+//     }
+
+// }
+
+// const CounterReducer = () => {
+//     console.log('Render CounterReducer');
+
+
+//     const [value, dispatch] = useReducer(reducer, 0)
+
+//     return (
+//         <div>
+//             <h1>Count : {value}</h1>
+//             <button onClick={() =>
+//                 dispatch('increment')
+//             }>Increment</button>
+//             <button onClick={() =>
+//                 dispatch('dicriment')
+//             }>Dicriment</button>
+//         </div>
+//     )
+// }
+
+const reducer = (state, action) => {
+
+    if (action === 'increment') {
         return state + 1
     } else {
         return state - 1
     }
-
 }
 
+
 const CounterReducer = () => {
-    console.log('Render CounterReducer');
-    
 
     const [value, dispatch] = useReducer(reducer, 0)
 
     return (
         <div>
             <h1>Count : {value}</h1>
-            <button onClick={() =>
-                dispatch('increment')
-            }>Increment</button>
-            <button onClick={() =>
-                dispatch('dicriment')
-            }>Dicriment</button>
+            <button onClick={() => { dispatch('increment') }}>Increase</button>
+            <button onClick={() => { dispatch('decrease') }}>Decrease</button>
         </div>
     )
+
+
 }
 
 
